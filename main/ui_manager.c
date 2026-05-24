f3314a973fa908c1f28cc0dda988b3d71c185ad1
fjtorresn <fco.torres12@gmail.com>
2026-05-24 02:24:06 -0400

    Refactor: replace Queue with Semaphore to handle button event

--- main/ui_manager.c
+++ main/ui_manager.c
@@ -10,19 +10,19 @@
 #include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
-//#include "freertos/semphr.h"
+#include "esp_log.h"
 #include "driver/gpio.h"
 #include "tasks_common.h"
 #include "ui_manager.h"
 
 
+static const char *TAG = "UI_MANAGER";
+
 static EventGroupHandle_t local_system_events = NULL;
 
 static uint8_t s_led_state = 0;
 
-static QueueHandle_t gpio_evt_queue = NULL;
-//static SemaphoreHandle_t gpio_evt_smph = NULL;
+static SemaphoreHandle_t gpio_evt_smph = NULL;
 
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
@@ -32,20 +32,18 @@ static void IRAM_ATTR gpio_isr_handler(void* arg)
 	uint32_t current_time = xTaskGetTickCountFromISR() * portTICK_PERIOD_MS;
 	
 	if (current_time - last_registered_time > DEBOUNCE_TIME_MS) {
-	    uint32_t gpio_num = (uint32_t) arg;
-	    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+		xSemaphoreGiveFromISR(gpio_evt_smph, NULL);
 		last_registered_time = current_time;
 	}
 }
 
 static void ui_manager_task(void* arg)
 {
-    uint32_t io_num;
     for (;;) {
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+        if (xSemaphoreTake(gpio_evt_smph, portMAX_DELAY)) {
 			gpio_set_level(BLINK_OUTPUT_GPIO, s_led_state);
 			s_led_state = !s_led_state;
-            printf("GPIO[%"PRIu32"] val: %d\n", io_num, gpio_get_level(io_num));
+			ESP_LOGI(TAG, "Botón presionado");
 			xEventGroupSetBits(local_system_events, RING_PRESSED_BIT);
         }
     }
@@ -71,7 +69,7 @@ void ui_manager_start(EventGroupHandle_t system_events) {
 	gpio_isr_handler_add(BUTTON_INPUT_GPIO, gpio_isr_handler, (void*) BUTTON_INPUT_GPIO);
 
 	// Inter process communication
-	gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+	gpio_evt_smph = xSemaphoreCreateBinary();
 	local_system_events = system_events;
 
 	// Task
